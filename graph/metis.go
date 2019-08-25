8c7649c9a7c75f7846af43a8ec527bb5ed03d659
Dickson Tsai <dickson.tsai@berkeley.edu>
2019-08-24 18:58:49 -0700

    Remove cgo stuff

--- graph/metis.go
+++ graph/metis.go
@@ -2,16 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !windows,!darwin
-
 package graph
 
-/*
-#cgo linux CFLAGS: -O2
-#cgo linux LDFLAGS: -L/usr/lib -L/usr/local/lib -lmetis
-
-#include <metis.h>
-*/
 import "C"
 
 import (
